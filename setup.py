06f07a729054fb7c7b8f990a36299e4948fb947b
LucaBonamino <luca.bonamino@hotmail.com>
2025-05-18 12:41:24 +0200

    python project setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+version = "0.1.0"
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+
+setup(
+        name="PyRDB",
+        url="https://github.com/LucaBonamino/PyRDB.git",
+        author="LucaBonamino",
+        version=version,
+        desciption="A dummy relational database in Python",
+        packages=find_packages(where="src", include=("PyRDB,"), exclude=("tests",)),
+        package_dir={"": "src"},
+        install_requires=requirements
+)
